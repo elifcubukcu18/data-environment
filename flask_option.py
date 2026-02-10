a097e3f5796ec7efa4cfc62a07cb971f059005f7
Elif Çubukcu <elifcubukcu@192.168.1.101>
2026-02-10 14:04:42 +0300

    Implement start() using FLASK_ENV

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,13 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    env = os.getenv("FLASK_ENV")
+    if env == "production":
+        return "Running in production mode"
+    elif env == "development":
+        return "Running in development mode"
+    else:
+        return "Running in unkown mode"
 
 if __name__ == "__main__":
     print(start())
